a870576e9b19e7d902855b15494c85bc619f67c3
David Chen <darkwei233@gmail.com>
2026-01-09 20:07:45 -0800

    [google_maps_flutter_web] Add WebAssembly (Wasm) support
    Make the Registrar parameter nullable in registerWith() to allow
    plugin registration in Wasm mode, where the registrar is null.

--- packages/google_maps_flutter/google_maps_flutter_web/CHANGELOG.md
+++ packages/google_maps_flutter/google_maps_flutter_web/CHANGELOG.md
@@ -1,6 +1,7 @@
 ## NEXT
 
 * Updates minimum supported SDK version to Flutter 3.35/Dart 3.9.
+* Adds support for WebAssembly (Wasm) compilation.
 
 ## 0.5.14+3
 
--- packages/google_maps_flutter/google_maps_flutter_web/lib/src/google_maps_flutter_web.dart
+++ packages/google_maps_flutter/google_maps_flutter_web/lib/src/google_maps_flutter_web.dart
@@ -9,7 +9,7 @@ part of '../google_maps_flutter_web.dart';
 /// This class implements the `package:google_maps_flutter` functionality for the web.
 class GoogleMapsPlugin extends GoogleMapsFlutterPlatform {
   /// Registers this class as the default instance of [GoogleMapsFlutterPlatform].
-  static void registerWith(Registrar registrar) {
+  static void registerWith(Registrar? registrar) {
     GoogleMapsFlutterPlatform.instance = GoogleMapsPlugin();
   }
 
